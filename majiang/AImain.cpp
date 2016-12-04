1a4420d1cb9ede9e908a2266f291451fc5edfdec
Roy08 <Roy08@mail.ustc.edu.cn>
2016-12-04 12:44:29 +0800

    v1.3

---
+++ majiang/AImain.cpp
@@ -0,0 +1,77 @@
+#include<iostream>
+#include<stdio.h>
+#include<string>
+#include<math.h>
+#include"majiangAI.h"
+
+using namespace std;
+
+
+int main()
+{
+	int a = 1;
+	AI player(a);
+	cout << player.respond_table("0b10") << endl;
+	cout << player.respond_table("0b30") << endl;
+	cout << player.respond_table("0b40") << endl;
+	cout << player.respond_table("0t20") << endl;
+	cout << player.respond_table("0t50") << endl;
+	cout << player.respond_table("0t60") << endl;
+	cout << player.respond_table("0t60") << endl;
+	cout << player.respond_table("0t60") << endl;
+	cout << player.respond_table("0t90") << endl;
+	cout << player.respond_table("0t90") << endl;
+	cout << player.respond_table("0w20") << endl;
+	cout << player.respond_table("0w20") << endl;
+	cout << player.respond_table("0w5o") << endl;
+	cout << player.respond_table("0t20") << endl;
+	cout << player.respond_table("0w20") << endl;
+	cout << player.respond_table("0w50") << endl;
+
+	cout << player.respond_table("2t91") << endl;
+	cout << player.respond_table("1t94") << endl;
+
+	cout << player.respond_table("2t61") << endl;
+	cout << player.respond_table("1t66") << endl;
+
+	cout << player.respond_table("0t90") << endl;
+	cout << player.respond_table("1t95") << endl;
+
+	cout << player.respond_table("0w20") << endl;
+	cout << player.respond_table("0t30") << endl;
+	cout << player.respond_table("3t11") << endl;
+
+
+	system("pause");
+
+}
+//"0b10"
+//"0b20"
+//"0b30"
+//"0b40"
+//"0b50"
+//"0b60"
+//"0b70"
+//"0b80"
+//"0b90"
+//"0t10"
+//"0t20"
+//"0t30"
+//"0t40"
+//"0t50"
+//"0t60"
+//"0t70"
+//"0t80"
+//"0t90"
+//"0w10"
+//"0w20"
+//"0w30"
+//"0w40"
+//"0w50"
+//"0w60"
+//"0w70"
+//"0w80"
+//"0w90"
+//
+
+
--- majiang/majiangAI.cpp
+++ majiang/majiangAI.cpp
@@ -2,47 +2,10 @@
 #include<stdio.h>
 #include<string>
 #include<math.h>
+#include"majiangAI.h"
+
 using namespace std;
 
-class AI
-{
-public:
-	string respond_table(string message);      //接受信息返回信息的，最高层函数
-	AI(int);
-	
-
-private:
-	bool hupai(int hand_brick[], int chupai);    //将自己的牌，与别人的出牌判断是否胡牌
-	bool hupai(int hand_brick[]);				//基本同上
-	void tingpai(int hand_brick[]);				//判断自己听哪张牌，将听得牌都返回
-	bool pengpai(int hand_brick[], int chupai);	//判断是否要碰牌
-	bool zhigangpai(int hand_brick[], int chupai);	//判断自己是否要直杠牌
-	bool wangangpai(int hand_brick[],  int mopai);  //判断自己是否要弯杠牌
-	int angangpai(int hand_brick[]);
-	double calculate_energy(int hand_brick[]);   //计算当前牌面的势能函数
-	double calculate_energy_max(int hand_brick[]);  //计算当前牌面不论出哪张牌，能达到的最大势能
-	int remain(int hand_brick[]);				//计算当前牌组剔除后还有几张，是胡牌函数的小函数
-	int chupai(int hand_brick[]);				//判断该出哪张牌
-	void set_quepai(int hand_brick[]);
-
-
-	int m_hand_brick[30];						//手牌数组，较少时，如13,10,7,4,1
-	int m_clear_brick[30];							//自己的桌面碰杠牌
-	int m_tingpai[30];
-	int m_guopai = 0;
-	int m_jiang_num=0;
-	int m_chupai_other;
-	int m_chupai_self;
-	int m_identity;
-	double m_daque[30];                              //打缺权重
-	int m_anpai[30];								//剩下的还从来没见过的牌
-		//后面的暂时还没用上
-	int m_desk_brick[30];							//桌面上已经出现了的牌
-	int m_xia_chupai[30];							//三个对手出的牌
-	int m_dui_chupai[30];
-	int m_shang_chupai[30];
-
-};
 
 
 string AI::respond_table(string message)
@@ -574,73 +537,5 @@ int AI::angangpai(int hand_brick[])
 	if (energy_no > energy_gang)  return false;
 	else   return true;
 }
-
-
-int main()
-{
-	int a = 1;
-	AI player(a);
-	cout << player.respond_table("0b10")<<endl;
-	cout << player.respond_table("0b30") << endl;
-	cout << player.respond_table("0b40") << endl;
-	cout << player.respond_table("0t20") << endl;
-	cout << player.respond_table("0t50") << endl;
-	cout << player.respond_table("0t60") << endl;
-	cout << player.respond_table("0t60") << endl;
-	cout << player.respond_table("0t60") << endl;
-	cout << player.respond_table("0t90") << endl;
-	cout << player.respond_table("0t90") << endl;
-	cout << player.respond_table("0w20") << endl;
-	cout << player.respond_table("0w20") << endl;
-	cout << player.respond_table("0w5o") << endl;
-	cout << player.respond_table("0t20") << endl;
-	cout << player.respond_table("0w20") << endl;
-	cout << player.respond_table("0w50") << endl;
-
-	cout << player.respond_table("2t91") << endl;
-	cout << player.respond_table("1t94") << endl;
-
-	cout << player.respond_table("2t61") << endl;
-	cout << player.respond_table("1t66") << endl;
-
-	cout << player.respond_table("0t90") << endl;
-	cout << player.respond_table("1t95") << endl;
-
-	cout << player.respond_table("0w20") << endl;
-	cout << player.respond_table("0t30") << endl;
-	cout << player.respond_table("3t11") << endl;
-
-
-	system("pause");
-
-}
-//"0b10"
-//"0b20"
-//"0b30"
-//"0b40"
-//"0b50"
-//"0b60"
-//"0b70"
-//"0b80"
-//"0b90"
-//"0t10"
-//"0t20"
-//"0t30"
-//"0t40"
-//"0t50"
-//"0t60"
-//"0t70"
-//"0t80"
-//"0t90"
-//"0w10"
-//"0w20"
-//"0w30"
-//"0w40"
-//"0w50"
-//"0w60"
-//"0w70"
-//"0w80"
-//"0w90"
-//
 
 
